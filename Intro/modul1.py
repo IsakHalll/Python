7e95e210e5d9a1c24d06e952b161d542480dbddb
IsakHalll <isak.akerberg@elev.ga.ntig.se>
2023-08-24 13:01:20 +0200

    aaa

---
+++ Intro/modul1.py
@@ -0,0 +1,52 @@
+
+'''
+x = input("namn: ")
+age = input("ålder: ")
+
+print(x)
+print(age)
+
+print ("Hej", x, "du är", age, "år")
+
+
+print("""
+      hej klassen
+      detta är flerradig print""")
+
+print("resultatet:\n\n teinf22b vann")
+
+'''
+
+print("hello \"world\"")
+
+print("""
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+   """)
+
+print("123\n456")
+
+print("fy" + "17!")
+
+x=("123") 
+print("banan" , x )
+
+klass = input("class: ")
+print(klass)
+
+
+
+print("Välkommen\nskriv namn och ålder")
+a = input("namn: ")
+b = input("ålder: ")
+
+print(a,b)
+
