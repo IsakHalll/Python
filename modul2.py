6e2dd606179d662d1fab11be7ba8ccfc90d6fdd7
IsakHalll <isak.akerberg@elev.ga.ntig.se>
2023-08-29 09:30:14 +0200

    aaa

---
+++ modul2.py
@@ -0,0 +1,28 @@
+"""
+print(1+2)
+print(2-1)
+print(2/1)
+print(2*1)
+print(2**1)
+print(2//1)
+
+
+a=int(input()) #miniräknare som multiplicerar två tal
+b=int(input())
+
+print(a*b)
+
+
+a2=float(input("din längd i meter:")) #bmi räknare
+b2=float(input("din vikt:"))
+
+print(b2/(a2**2))
+
+
+a3=int(input("din ålder: "))    #ålder i verckor
+print("din ådler i veckor:", a3*52)
+"""
+
+a4=int(input("Kg: "))
+print(a4*2.20462262185)
+
