f70b00767d4fdcb66370d4c3d796891c75a5b0ef
IsakHalll <isak.akerberg@elev.ga.ntig.se>
2023-08-29 09:29:34 +0200

    aaa

---
+++ modul3.py
@@ -0,0 +1,18 @@
+import os
+
+os.system('cls')
+"""
+x=input("")
+
+for i in range(10):
+    print(x)
+"""
+"""
+y=1
+while True:
+    print(y)
+    if y == 10:
+        break
+    y += 1
+"""
+    
